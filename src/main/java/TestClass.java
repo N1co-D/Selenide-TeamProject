aafa421ecadf5b520fbcabf7b3915d90509efa0e
Dmitriy <62928282+Tarasovdv@users.noreply.github.com>
2024-02-25 19:12:25 +0300

    modify (#6)
    Co-authored-by: Tarasovdv <desant74268@yande.ru>
    Co-authored-by: N1co-D <122123347+N1co-D@users.noreply.github.com>

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -3,7 +3,7 @@ public class TestClass {
         System.out.println("Пока что как-то так...");
         System.out.println("(С) Даниил");
         System.out.println("Tarasov");
-
+        System.out.println("Tarasov test 2");
         System.out.println("Test #2");
     }
 }
