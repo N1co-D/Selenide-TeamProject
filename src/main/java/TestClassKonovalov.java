593ff64e889d2a49d4338a450573efd48f136f76
Pavel <p666v@yandex.ru>
2024-02-25 19:15:01 +0300

    Проверка совместимости Коновалов П.В.

--- src/main/java/TestClassKonovalov.java
+++ src/main/java/TestClassKonovalov.java
@@ -1,5 +1,6 @@
 public class TestClassKonovalov {
     public static void main(String[] args) {
         System.out.println("Проверка... Автор Коновалов П.В.");
+        System.out.println("Проверка... Автор Коновалов П.В.");
     }
 }
