9dd45c2a715ac1405bdbd5372e266ccdb7967148
Tarasovdv <desant74268@yande.ru>
2024-02-25 19:09:14 +0300

    modify

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -3,5 +3,6 @@ public class TestClass {
         System.out.println("Пока что как-то так...");
         System.out.println("(С) Даниил");
         System.out.println("Tarasov");
+        System.out.println("Tarasov test 2");
     }
 }
