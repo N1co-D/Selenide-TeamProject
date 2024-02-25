0ec07830eacf3da657fc0c5a658e61946ec6f9bd
N1co-D <122123347+N1co-D@users.noreply.github.com>
2024-02-25 19:10:33 +0300

    test #2 (#5)
    Co-authored-by: dmark <dmarkevich@gmail.com>

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -3,5 +3,7 @@ public class TestClass {
         System.out.println("Пока что как-то так...");
         System.out.println("(С) Даниил");
         System.out.println("Tarasov");
+
+        System.out.println("Test #2");
     }
 }
