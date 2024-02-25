94c256ce12db0f6043b541038cb8c3be07230d0a
dmark <dmarkevich@gmail.com>
2024-02-25 19:04:51 +0300

    changes

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -2,6 +2,6 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Пока что как-то так...");
         System.out.println("(С) Даниил");
-              System.out.println("Tarasov");
+        System.out.println("Tarasov");
     }
 }
