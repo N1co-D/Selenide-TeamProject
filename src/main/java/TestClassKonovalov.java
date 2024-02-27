b1c6ec844e4bb82097db26a9d0be864bb4ff7957
N1co-D <122123347+N1co-D@users.noreply.github.com>
2024-02-27 22:38:09 +0300

    add gitignore (#11)
    * add gitignore
    
    * add gitignore
    
    * Delete test classes
    Changed dependencies: TestNG > JUnit
    
    * Delete test classes
    
    ---------
    
    Co-authored-by: dmark <dmarkevich@gmail.com>

--- src/main/java/TestClassKonovalov.java
+++
@@ -1,7 +0,0 @@
-public class TestClassKonovalov {
-    public static void main(String[] args) {
-        System.out.println("Проверка... Автор Коновалов П.В.");
-        System.out.println("Проверка... Автор Коновалов П.В.");
-        System.out.println("Проверка... Автор Коновалов П.В.");
-    }
-}
