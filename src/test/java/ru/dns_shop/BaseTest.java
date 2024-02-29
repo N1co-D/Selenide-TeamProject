4caf6291af0b72f9d2718f8710ee59958bc9f11f
Tarasovdv <desant74268@yande.ru>
2024-02-29 20:18:55 +0300

    modify BaseTest.java

--- src/test/java/ru/dns_shop/BaseTest.java
+++ src/test/java/ru/dns_shop/BaseTest.java
@@ -3,17 +3,17 @@ package ru.dns_shop;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import ru.dns_shop.utilities.ConfProperties;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class BaseTest {
 
     @BeforeAll
     public static void installTestConfigurations() {
-        Configuration.browser = "chrome";
+        Configuration.browser = new ConfProperties().getProperty("chrome");
         Configuration.browserSize = null;
         DesiredCapabilities cap = new DesiredCapabilities();
         ChromeOptions options = new ChromeOptions();
