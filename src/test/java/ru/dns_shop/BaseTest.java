4884c73609d5437e53488b36d4af9e3d1c97f06d
Tarasovdv <desant74268@yande.ru>
2024-02-29 10:27:13 +0300

    modify BaseTest.java

--- src/test/java/ru/dns_shop/BaseTest.java
+++ src/test/java/ru/dns_shop/BaseTest.java
@@ -3,18 +3,22 @@ package ru.dns_shop;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class BaseTest {
 
     @BeforeAll
     public static void setupClass() {
         Configuration.browser = "chrome";
-        open();
-        getWebDriver().manage().window().maximize();
+        Configuration.browserSize = null;
+        DesiredCapabilities cap = new DesiredCapabilities();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        cap.setCapability(ChromeOptions.CAPABILITY, options);
+        Configuration.browserCapabilities = cap;
     }
 
     @AfterAll
