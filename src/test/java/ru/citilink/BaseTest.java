58c4ba198efae80e1eb0575a7019986de90c2399
dmark <dmarkevich@gmail.com>
2024-03-06 19:16:34 +0300

    dns_shop -> citilink

---
+++ src/test/java/ru/citilink/BaseTest.java
@@ -0,0 +1,30 @@
+package ru.citilink;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import ru.citilink.utilities.ConfProperties;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class BaseTest {
+
+    @BeforeEach
+    public void installTestConfigurations() {
+        Configuration.browser = new ConfProperties().getProperty("browser-name");
+        Configuration.browserSize = null;
+        Configuration.pageLoadStrategy = "none";
+        DesiredCapabilities cap = new DesiredCapabilities();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        cap.setCapability(ChromeOptions.CAPABILITY, options);
+        Configuration.browserCapabilities = cap;
+    }
+
+    @AfterEach
+    public void quitTest() {
+        closeWebDriver();
+    }
+}
