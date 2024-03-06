493ae7b57c74f2cbe141ae21796eb42632a57ca9
dmark <dmarkevich@gmail.com>
2024-03-06 18:52:13 +0300

    [Daniil] Test 1 Add constants

--- src/test/java/ru/dns_shop/BruceLeeTest.java
+++ src/test/java/ru/dns_shop/BruceLeeTest.java
@@ -10,18 +10,17 @@ import ru.dns_shop.utilities.ConfProperties;
 import static com.codeborne.selenide.Selenide.open;
 
 public class BruceLeeTest extends BaseTest {
+    MainPage mainPage = new MainPage();
+    ResultsPage resultsPage = new ResultsPage();
+    CartPage cartPage = new CartPage();
     private static final String MAIN_PAGE = new ConfProperties().getProperty("test-site");
 
     @Test
     public void checkTheIncreaseInQuantityWhenAddingProductsToCart() {
-        MainPage mainPage = new MainPage();
-
         open(MAIN_PAGE);
         Assertions.assertTrue(mainPage.getPagesUniqueElement(),
                 "Ошибка в открытии главной страницы");
         String inputText = "Ноутбук Huawei MateBook D 14 53013XFA, 14";
-
-        ResultsPage resultsPage = new ResultsPage();
 
         mainPage.SearchGameByInputBox(inputText);
         Assertions.assertTrue(resultsPage.getPagesUniqueElement(),
@@ -39,8 +38,6 @@ public class BruceLeeTest extends BaseTest {
         Assertions.assertTrue(resultsPage
                         .checkDisappearingWindowWithAddedProductInCartStatus(),
                 "Ошибка в закрытии всплывающего окна с сообщением о добавлении товара в корзину");
-
-        CartPage cartPage = new CartPage();
 
         resultsPage.cartButtonClick();
         Assertions.assertTrue(cartPage.getPagesUniqueElement(),
