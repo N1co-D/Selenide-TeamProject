71876ee9da57a853b6125e112bb590bb14c20411
dmark <dmarkevich@gmail.com>
2024-03-07 12:30:26 +0300

    [Daniil] [Test 2] Add @ParameterizedTest

--- src/test/java/ru/citilink/BruceLeeTest.java
+++ src/test/java/ru/citilink/BruceLeeTest.java
@@ -1,7 +1,8 @@
 package ru.citilink;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.citilink.pages.ComparingPage;
 import ru.citilink.pages.MainPage;
 import ru.citilink.pages.ResultsPage;
@@ -11,22 +12,20 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class BruceLeeTest extends BaseTest {
     MainPage mainPage = new MainPage();
+    ResultsPage resultsPage = new ResultsPage();
     ComparingPage comparingPage = new ComparingPage();
     private static final String MAIN_PAGE = new ConfProperties().getProperty("test-site");
 
-    @Test
-    public void checkTheAdditionOfProductToCompareSection() {
+    @ParameterizedTest
+    @CsvSource({"2"})
+    public void checkTheAdditionOfProductToCompareSection(int amountOfProductsForAdding) {
         open(MAIN_PAGE);
         Assertions.assertTrue(mainPage.getPagesUniqueElement(),
                 "Ошибка в открытии главной страницы");
 
-        ResultsPage resultsPage = new ResultsPage();
-
         mainPage.productCatalogClick().televisionsAndAudioVideoEquipmentCategoryClick().oledTelevisionsCategoryClick();
         Assertions.assertTrue(resultsPage.getPagesUniqueElement(),
                 "Ошибка в открытии страницы с результатами поиска");
-
-        int amountOfProductsForAdding = 2;
 
         resultsPage.enableDetailedCatalogMode().someProductAddToComparingClick(amountOfProductsForAdding);
         Assertions.assertEquals(resultsPage.getAmountOfAddedProductsToCompare(),
