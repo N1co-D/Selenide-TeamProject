c99a02c3199b9b1d813a83bff7b466d87540dc1b
dmark <dmarkevich@gmail.com>
2024-03-07 12:39:52 +0300

    [Daniil] [Test 4] Add @ParameterizedTest

--- src/test/java/ru/citilink/BruceLeeTest.java
+++ src/test/java/ru/citilink/BruceLeeTest.java
@@ -1,7 +1,8 @@
 package ru.citilink;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.citilink.pages.CartPage;
 import ru.citilink.pages.MainPage;
 import ru.citilink.pages.ResultsPage;
@@ -15,14 +16,14 @@ public class BruceLeeTest extends BaseTest {
     CartPage cartPage = new CartPage();
     private static final String MAIN_PAGE = new ConfProperties().getProperty("test-site");
 
-    @Test
-    public void checkingTheDeletingOfProductFromCart() {
+    @ParameterizedTest
+    @CsvSource({"'Переходники', 'Переходники на евровилку', " +
+            "'Адаптер-переходник на евровилку PREMIER 11626/20, темно-серый'"})
+    public void checkingTheDeletingOfProductFromCart(String inputText, String productFromDropDownList,
+                                                     String observedProduct) {
         open(MAIN_PAGE);
         Assertions.assertTrue(mainPage.getPagesUniqueElement(),
                 "Ошибка в открытии главной страницы");
-        String inputText = "Переходники";
-        String productFromDropDownList = "Переходники на евровилку";
-        String observedProduct = "Адаптер-переходник на евровилку PREMIER 11626/20, темно-серый";
 
         mainPage.inputBoxWriteText(inputText).clickOnProductFromDropDownList(productFromDropDownList);
         Assertions.assertTrue(resultsPage.getPagesUniqueElement(),
