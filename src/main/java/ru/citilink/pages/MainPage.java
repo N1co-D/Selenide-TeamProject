bd4c7c1b15998116c96a53bc0ed518dc8ff76a55
Konovalov Pavel <108332615+p666v@users.noreply.github.com>
2024-03-12 14:54:13 +0300

    Тест кейс ID3 (#18)
    * Тест кейс ID3
    
    * Правки и добавление @ParameterizedTest
    
    * Правки замечаний
    
    * Правки замечаний
    
    ---------
    
    Co-authored-by: Pavel <p666v@yandex.ru>

--- src/main/java/ru/citilink/pages/MainPage.java
+++ src/main/java/ru/citilink/pages/MainPage.java
@@ -4,9 +4,10 @@ import com.codeborne.selenide.ex.ElementNotFound;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
 
-import java.time.Duration;
-
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 import static org.assertj.core.api.Assertions.fail;
 
@@ -16,6 +17,9 @@ import static org.assertj.core.api.Assertions.fail;
 public class MainPage extends BasePage {
     private final String uniqueElement = "//div[@data-meta-name='BannersLayout']";
     private final String inputBox = "//input[@type='search']";
+    private final String searchDropDownList = "//div[@data-meta-name='InstantSearchExtraResultList']//a";
+    private final String compareButton = "//div[@data-meta-name='HeaderBottom__search']/..//div[@data-meta-name='CompareButton']";
+    private final String compareValue = "//div[@data-meta-name='HeaderBottom__search']/..//div[@data-meta-name='NotificationCounter']";
 
     public boolean getPagesUniqueElement() { //todo поменять имя
         try {
@@ -27,7 +31,7 @@ public class MainPage extends BasePage {
         return false;
     }
 
-    private MainPage inputBoxWriteText(String searchingProduct) {
+    public MainPage inputBoxWriteText(String searchingProduct) {
         jsClick($x(inputBox));
         $x(inputBox).sendKeys(searchingProduct);
         return this;
@@ -36,5 +40,20 @@ public class MainPage extends BasePage {
     public void searchProductByInputBox(String searchingProduct) {
         inputBoxWriteText(searchingProduct);
         $x(inputBox).should(visible, WAITING_TIME).pressEnter();
+    }
+
+    public ResultsPage productSearchExtraResultListClick(String gameName) {
+        $$x(searchDropDownList).shouldBe(sizeGreaterThan(0), WAITING_TIME)
+                .findBy(text(gameName)).click();
+        return new ResultsPage();
+    }
+
+    public boolean compareValueIsDisplayed() {
+        return $x(compareValue).shouldBe(visible, WAITING_TIME).isDisplayed();
+    }
+
+    public ComparePage compareButtonClick() {
+        $x(compareButton).shouldBe(visible, WAITING_TIME).click();
+        return new ComparePage();
     }
 }
