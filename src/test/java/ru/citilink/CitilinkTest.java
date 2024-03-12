671ace246218f04bbcffd1839f27597e751a1714
N1co-D <122123347+N1co-D@users.noreply.github.com>
2024-03-12 23:14:41 +0300

    [JIRA-1002] [ТC-ID8] Check the addition of product to compare section (#22)
    * [Daniil] Test 2
    
    * [Daniil] Test 2
    Add constants
    
    * dns_shop -> citilink
    
    * [Daniil] [Test 2] Add @ParameterizedTest
    
    * [Daniil] [Test 2] Corrections
    
    * [JIRA-1050] [ТК-ID8] [Даниил]
    Изменение имени
    
    * [JIRA-1002] [ТК-ID8] [Даниил]
    Комплексное исправление ошибок
    
    * [JIRA-1002] [ТК-ID8] [Даниил]
    Изменение формата цикла
    
    * [JIRA-1002] [ТК-ID8] [Даниил]
    Изменение аннотации
    
    * [JIRA-1002] [ТК-ID8] [Даниил]
    Исправление структуры проверок в методах
    
    * [JIRA-1002] [ТК-ID8] [Даниил]
    Решение конфликта
    
    * [JIRA-1002] [ТК-ID8] [Даниил]
    Решение конфликта
    
    * [JIRA-1002] [ТК-ID8] [Даниил]
    Решение конфликта
    
    * [JIRA-1002] [ТК-ID8] [Даниил]
    Решение конфликта
    
    ---------
    
    Co-authored-by: dmark <dmarkevich@gmail.com>

--- src/test/java/ru/citilink/CitilinkTest.java
+++ src/test/java/ru/citilink/CitilinkTest.java
@@ -2,6 +2,7 @@ package ru.citilink;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import ru.citilink.pages.CartPage;
 import ru.citilink.pages.ComparePage;
 import ru.citilink.pages.MainPage;
@@ -115,5 +116,25 @@ public class CitilinkTest extends BaseTest {
         assertAll(
                 () -> assertFalse(comparePage.compareValueIsDisplayed(), "Товар не удалён из сравнение"),
                 () -> assertTrue(comparePage.noProductsForCompareIsDisplayed(), "Отсутствует уведомление Нет товаров для сравнения"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2})
+    public void checkTheAdditionOfProductToCompareSection(int amountOfProductsForAdding) {
+        open(confProperties.getProperty("test-site"));
+
+        mainPage.checkIfCorrectPageOpen()
+                .productCatalogClick()
+                .televisionsAndAudioVideoEquipmentCategoryClick()
+                .oledTelevisionsCategoryClick();
+
+        resultsPage.checkIfCorrectPageOpen()
+                .enableDetailedCatalogMode()
+                .someProductAddToComparingClick(amountOfProductsForAdding)
+                .checkAmountOfAddedProductsToCompare(amountOfProductsForAdding)
+                .comparingButtonClick();
+
+        comparePage.checkIfCorrectPageOpen()
+                .checkAmountOfAddedProductsToCompare(amountOfProductsForAdding);
     }
 }
