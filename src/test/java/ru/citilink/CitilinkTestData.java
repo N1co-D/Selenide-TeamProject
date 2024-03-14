1e87f96593b065ff37d9a0fcd1da8ed90fc934d7
Pavel <p666v@yandex.ru>
2024-03-14 22:18:09 +0300

    fix

--- src/test/java/ru/citilink/CitilinkTestData.java
+++ src/test/java/ru/citilink/CitilinkTestData.java
@@ -6,8 +6,8 @@ import java.util.stream.Stream;
 
 public class CitilinkTestData {
     static Stream<Arguments> dataForComparisonTest() {
-        return Stream.of(
-                Arguments.of("Ноутбук Lenovo IdeaPad 1 15AMN7 82VG00LSUE, 15.6\", TN, AMD Ryzen 3 7320U, 4-ядерный, 8ГБ LPDDR5, 256ГБ SSD, AMD Radeon 610M, серый", "Ноутбуки Lenovo")
-        );
+        String testLaptop = "Ноутбук Lenovo IdeaPad 1 15AMN7 82VG00LSUE, 15.6\", TN, AMD Ryzen 3 7320U, 4-ядерный, 8ГБ LPDDR5, 256ГБ SSD, AMD Radeon 610M, серый";
+        String productCategory = "Ноутбуки Lenovo";
+        return Stream.of(Arguments.of(testLaptop, productCategory));
     }
 }
